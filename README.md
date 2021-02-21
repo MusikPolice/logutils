c5103bc48b5809616a194a5098c92bc44d7eb518
MusikPolice <jonfritz@gmail.com>
2021-02-21 13:43:45 -0500

    Added more tags

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # logutils
 Utilities for generating and converting between common Amateur Radio log file formats
+
+## Cabrillo Support
+The Cabrillo spec is documented here: https://wwrof.org/cabrillo/
--- src/main/kotlin/ca/jonathanfritz/logutils/ca/jonathanfritz/logutils/cabrillo/CabrilloTag.kt
+++ src/main/kotlin/ca/jonathanfritz/logutils/ca/jonathanfritz/logutils/cabrillo/CabrilloTag.kt
@@ -1,10 +1,22 @@
 package ca.jonathanfritz.logutils.ca.jonathanfritz.logutils.cabrillo
 
+// from https://wwrof.org/cabrillo/cabrillo-v3-header/
 enum class CabrilloTag(public val tag: String, val required: Boolean, val pattern: Regex = Regex(".*")) {
-    // from https://wwrof.org/cabrillo/cabrillo-v3-header/
+
+    // major.minor ex: 3.0
     StartOfLog("START-OF-LOG", true, Regex("""^\d+\.?\d*$""")),
+
+    // no value
     EndOfLog("END-OF-LOG", true, Regex("""^$""")),
+
+    // one or two character alpha prefix, a separating numeral, one to four character alpha suffix
     Callsign("CALLSIGN", false, Regex("""^[A-Z]{1,2}\d[A-Z0-9]{2,4}$""")),
+
+    // valid characters are A-Z, 0-9, and hyphen (-). Maximum length is 32 characters
     Contest("CONTEST", false, Regex("""^[A-Z0-9\-]{1,32}$""")),
-    CategoryAssisted("CATEGORY-ASSISTED", false, Regex("""^(ASSISTED|NON-ASSISTED)$"""))
+
+    // one of two possible values
+    CategoryAssisted("CATEGORY-ASSISTED", false, Regex("""^(ASSISTED|NON-ASSISTED)$""")),
+
+    CategoryBand("CATEGORY-BAND", false, Regex("""^()$"""))
 }
